1f2b34b09609283051258ab0ed4f4da5662ff48a
ozidrice <trolarddamien@gmail.com>
2018-04-24 11:40:53 +0200

    Ajout fonction has_right

--- app/Utilisateur.php
+++ app/Utilisateur.php
@@ -26,4 +26,30 @@ class Utilisateur extends Model implements AuthenticatableContract, Authorizable
      * @var array
      */
     protected $hidden = ['idNiveau', 'actif', 'pass'];
+
+
+    private static $admin_right = []; 
+    private static $user_right = []; 
+    /*
+    *   Return true if the user has the right to ...
+    *   @returns bool
+    */
+    public function has_right(string $right){
+        switch($this->idNiveau){
+            case 1: 
+                //SuperAdmin  
+                return true;
+            case 2:
+                //Admin
+                if(in_array($right, $admin_right))
+                    return true;
+                return false;
+            case 3:
+                //User
+                if(in_array($right, $user_right))
+                    return true;
+                return false;
+            }
+        return false;
+    }
 }
