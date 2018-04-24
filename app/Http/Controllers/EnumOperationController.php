560abeffbfab93b78e9dec4d8197f29a33dc4cad
Caori <naiko.genevaux@gmail.com>
2018-04-24 12:40:40 +0200

    +niv&enum

---
+++ app/Http/Controllers/EnumOperationController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EnumOperation;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+use Auth;
+
+
+class EnumOperationController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //fetch toutes les opérations
+    public function index() {
+         $operations = EnumOperation::all();
+         return response()->json($operations, 200);
+    }
+    //va chercher l'opération' avec l'id correspondant
+    public function getOperation($id) {
+         $operation = EnumOperation::findOrFail($id);
+         return response()->json($operation, 200);
+    }
+
+    //crée une nouvelle opération
+    public function saveOperation(Request $request) {
+         $this->validate($request, ["intitule" => 'required']);
+         $operation = EnumOperation::create($request->all());
+         $operation->save();
+         return response()->json($operation, 200);
+    }
+
+    //permet de modifier les informations d'une opération
+    public function updateOperation(Request $request, $id) {
+         $this->validate($request, ["intitule" => 'required']);
+         $operation = EnumOperation::findOrFail($id);
+         $operation->intitule = $request->input('intitule');
+         $operation->save();
+         return response()->json($operation, 200);
+    }
+
+    //suppr une opération
+    public function deleteOperation($id) {
+         $operation = EnumOperation::findOrFail($id);
+         $operation->delete();
+         return response()->json(['status' => 'Deleted'], 200);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,19 @@ $router->delete($lienAPI.'cast/{id}', [
      'as'   => 'del-cast',
      'uses' => 'CastController@deleteCast'
      ]);
+
+/*________NIVEAUUTILISATEUR_________*/
+
+$router->get($lienAPI.'niveaux', 'NiveauUtilisateurController@index');
+$router->get($lienAPI.'niveau/{id}',  ['as'   => 'niveau',	'uses' => 'NiveauUtilisateurController@getNiveau']);
+$router->post($lienAPI.'niveau', 'NiveauUtilisateurController@saveNiveau');
+$router->put($lienAPI.'niveau/{id}', ['as'   => 'upd-niveau',	'uses' => 'NiveauUtilisateurController@updateNiveau']);
+$router->delete($lienAPI.'niveau/{id}', ['as'   => 'del-niveau',	'uses' => 'NiveauUtilisateurController@deleteNiveau']);
+
+/*________ENUMOPERATION_________*/
+
+$router->get($lienAPI.'operations', 'EnumOperationController@index');
+$router->get($lienAPI.'operation/{id}',  ['as'   => 'operation',	'uses' => 'EnumOperationController@getOperation']);
+$router->post($lienAPI.'operation', 'EnumOperationController@saveOperation');
+$router->put($lienAPI.'operation/{id}', ['as'   => 'upd-operation',	'uses' => 'EnumOperationController@updateOperation']);
+$router->delete($lienAPI.'operation/{id}', ['as'   => 'del-operation',	'uses' => 'EnumOperationController@deleteOperation']);
