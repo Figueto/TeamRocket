404acb705341d00bce3fac665fd453b19c00d072
Aleqsian <alexiandu57@hotmail.fr>
2018-05-13 19:56:16 +0200

    Functions are more React

--- src/App2.js
+++ src/App2.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types'
 
-function Oeuvre({titre = 'Inconnu', date = 'Inconnu', auteur = 'Inconnu', genre = 'Inconnu', pays = 'Inconnu', resume = 'Inconnu'}) {
+function Oeuvre({titre, date, auteur, genre, pays, resume}) {
   return (
     <div className="Oeuvre">
       <p> Le titre est = {titre} </p>
@@ -15,14 +16,44 @@ function Oeuvre({titre = 'Inconnu', date = 'Inconnu', auteur = 'Inconnu', genre 
   )
 }
 
-function Statistique({temps = 'Inconnu', catégorie = 'Inconnu', Note = '_'}) {
+Oeuvre.defaultProps = {
+  titre: 'Inconnu',
+  date: 'Inconnu',
+  auteur: 'Inconnu',
+  genre: 'Inconnu',
+  pays: 'Inconnu',
+  resume: 'Inconnu', 
+}
+
+function Statistique({temps, catégorie, note}) {
   return (
     <div className="Oeuvre">
       <p> Vos catégories préférées sont = {catégorie} </p>
       <p> Le temps passé à visionner des oeuvre est = {temps} </p>
-      <p> Votre note moyenne est = {Note} </p>
+      <p> Votre note moyenne est = {note} </p>
     </div>
   )
+}
+
+Statistique.defaultProps = {
+  temps: 'Inconnu',
+  catégorie: 'Inconnu',
+  note: 'Inconnu',
+}
+
+function SearchBar({search}){
+  return(
+    <div className="SearchBar">
+        <form action="" class="formulaire">
+          <input class="champ" type="text" value={search}/>
+          <input class="bouton" type="button" value="Rechercher" />
+        </form>
+    </div>
+  )
+}
+
+SearchBar.propTypes = {
+  search: PropTypes.string.isRequired,
 }
 
 class App extends Component {
@@ -30,13 +61,7 @@ class App extends Component {
     return (
       <div className="App">
       <h1>DashBoard Team Rocket </h1>
-      <div id="searchbar">
-          <form action="" class="formulaire">
-             <input class="champ" type="text" value="Recherhez une oeuvre"/>
-            <input class="bouton" type="button" value="Rechercher " />
-
-                </form>
-                </div>
+      <SearchBar search='Rechercher une oeuvre' />
       <div className= "Grille">
         <div className= "Colonne">
           <h2> Terminés </h2>
@@ -58,7 +83,7 @@ class App extends Component {
         </div>
         <div className= "Colonne">
           <h2> Statistiques </h2>
-          <Statistique temps ="666h" catégorie = "Science-Fiction" Note = "6" />
+          <Statistique temps ="666h" catégorie = "Science-Fiction" note = "6" />
         </div>
       </div>
     </div>
