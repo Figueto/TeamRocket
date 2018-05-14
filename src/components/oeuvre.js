3cea043f7354be714f5ce3a252f15132328f22b6
Aleqsian <alexiandu57@hotmail.fr>
2018-05-14 12:38:42 +0200

    oeuvre css

--- src/components/oeuvre.js
+++ src/components/oeuvre.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './oeuvre.css'
 
 
 export default class Oeuvre extends React.Component {
@@ -17,6 +18,7 @@ export default class Oeuvre extends React.Component {
 		const  content = <div className='Oeuvre'>
 			{films.map(film => {
 				return <tr key={film.id}>
+				<div className = "oeuvre-wrap">
 				      <h4> Le titre est = {film.titre} </h4>
 				      <p className='Date'> La date est = {film.date} </p>
 				      <div className = 'InfosOeuvre'>
@@ -24,8 +26,9 @@ export default class Oeuvre extends React.Component {
 					      <p className = 'Genre'> Le genre est = {film.genre} </p>
 					      <p className = 'Pays'> Le pays est = {film.pays} </p>
 					      <p className = 'Resume'> Le resume est = {film.resume} </p>
-				      </div>
-			     </tr>
+				      </div>     
+			    </div>
+			    </tr>
 		 	})}
 	    </div>
 	    return content;
