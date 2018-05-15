53fc14420dea6ae5e49c39d39eaa2583057aaa74
Caori <naiko.genevaux@gmail.com>
2018-05-15 13:17:50 +0200

    auth updateUser done + issue recherche

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Crypt;
 use App\Utilisateur;
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\LogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\QueryException;
@@ -21,7 +20,7 @@ class UtilisateurController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['saveUtilisateur']]);
+    $this->middleware('auth', ['except' => ['saveUtilisateur']]);
         $this->middleware('admin', ['except' => ['saveUtilisateur', 'updateUtilisateur']]);
     }
 
@@ -40,31 +39,35 @@ class UtilisateurController extends Controller
     }
 
     //crée un nouvel utilisateur
+    //utilisable seulement par un user non connecté
     public function saveUtilisateur(Request $request) {
          $this->validate($request,
          ["pseudo" => 'required|unique:utilisateur',
          "mail" => 'required|unique:utilisateur',
          "pass" => 'required',
           "idNiveau" =>'required']);
+          //fonctionne pas. me laisse pas utiliser auth vu que j'ai exclu cette fonction du middleware 'auth' dans le construct
+          /*$idUtilisateur = $request->auth->idUtilisateur;
+          echo $idUtilisateur;
           //si user existe (connecté), stop
           if($request->auth != null) {
                abort(401, 'Vous êtes déjà connecté');
-          }
+          }*/
          $utilisateur = Utilisateur::create($request->all());
          $utilisateur->actif = 1;
+         $utilisateur->idNiveau = $request->input('idNiveau');
            //hashage mdp
            $utilisateur->pass = Crypt::encrypt($utilisateur->pass);
            $utilisateur->save();
-
-        LogController::save($request,1,3,$utilisateur->idUtilisateur);
-        return response()->json(["utilisateur"=>$utilisateur], 200);
+         return response()->json(["utilisateur"=>$utilisateur], 200);
     }
 
     //permet de modifier les informations d'un utilisateur
+    //si admin peut update n'imp quel user, sinon peut upd que lui-meme
     public function updateUtilisateur(Request $request, $id) {
          $this->validate($request, ["pseudo" => 'required', "mail" => 'required', "pass" => 'required']);
          //si pas admin
-         if(!estAdmin($request->auth)) {
+         if($request->auth->idUtilisateur == 1 || $request->auth->idUtilisateur == 2) {
               //verifier si l'id rentré est le sien
               if($id != $request->auth->idUtilisateur) {
                    abort(401, "Ce n'est pas votre compte, vous ne pouvez pas le modifier !");
@@ -77,7 +80,6 @@ class UtilisateurController extends Controller
          //hashage mdp
          $utilisateur->pass = Crypt::encrypt($utilisateur->pass);
          $utilisateur->save();
-        LogController::save($request,3,3,$id);
          return response()->json(["utilisateur"=>$utilisateur], 200);
     }
 
@@ -85,7 +87,6 @@ class UtilisateurController extends Controller
     public function deleteUtilisateur($id) {
          $utilisateur = Utilisateur::findOrFail($id);
          $utilisateur->delete();
-        LogController::save($request,2,3,$id);
          return response()->json(['status' => 'Deleted'], 200);
     }
 }
