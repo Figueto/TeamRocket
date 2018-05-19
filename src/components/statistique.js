e604184b41bce9a6730a1d9f24ebe2545147a352
Aleqsian <alexiandu57@hotmail.fr>
2018-05-19 15:27:42 +0200

    first changeState on statistique

--- src/components/statistique.js
+++ src/components/statistique.js
@@ -6,25 +6,34 @@ export default class Statistique extends React.Component {
 	constructor (props) {
 		super(props)
 		this.state = {
-			stats: [
-				{id: 1, temps: '783', categorie: 'Science-Fiction', note: '6'},
-			]
+			temps: 783,
+			categorie: 'Science-Fiction',
+			note: 6
 		}
+	}
+
+	changeNote() {
+		if(this.state.note==10){
+			this.setState({
+    			note : 0
+  			})
+		}
+		else{
+  			this.setState({
+    			note : this.state.note+1
+  			})
+  		}
 	}
 
 	render() {
 		const {stats} = this.state;
 		const content = <div className="Oeuvre">
 			<h2> Statistiques </h2>
-			{stats.map(stat => {
-				return <tr key={stat.id}>
 				<div className='stats-wrapper'>
-	      			<p> Votre catégorie préférée est : {stat.categorie} </p>
-	      			<p> Vous avez regardé des films pendant {stat.temps} heures </p>
-	     			<p> Votre note moyenne est : {stat.note} </p>
+	      			<p className='Categorie'> Votre catégorie préférée est : {this.state.categorie} </p>
+	      			<p className='temps'> Vous avez regardé des films pendant {this.state.temps} heures </p>
+	     			<p className='note' onClick={this.changeNote.bind(this)}> Votre note moyenne est : {this.state.note} </p>
      			</div>
-     			</tr>
-    		 })}
    		 </div>
 	    return content;
 	}
