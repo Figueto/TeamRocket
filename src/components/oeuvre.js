5e7f9fb3d69c65be16b49ee3166de82ade6f9225
Aleqsian <alexiandu57@hotmail.fr>
2018-05-21 00:03:12 +0200

    Correction files

--- src/components/oeuvre.js
+++ src/components/oeuvre.js
@@ -7,33 +7,20 @@ import Bouton from './Bouton.js'
 
 export default class Oeuvre extends React.Component {
 
-	render() {
-		const {films} = this.state;
-		const  content = <div className='Oeuvre'>
-			{films.map(film => {
-				return <tr key={film.id}>
-				<div className = "oeuvre-wrap">
-				      <p className='Title'> {film.titre} </p>
-				      <div className = 'InfosOeuvre'>
-							<div className ='InfosImage'>
-								<Image/>
-							</div>
-								<div className = 'InfosTexte'>
-								<p className='Date'> {film.date} </p>
-					      	<p className = 'Auteur'> Auteur : {film.auteur} </p>
-					      	<p className = 'Genre'>  Genre : {film.genre} </p>
-					      	<p className = 'Pays'> Origine :{film.pays} </p>
-					      	<p className = 'Resume'> Resume : {film.resume} </p>
-								</div>
-								<div className ='Boutons'>
-									<Bouton/>
-								</div>
-				      </div>
-			    	</div>
-			    </tr>
-		 	})}
-	    </div>
-	    return content;
+	constructor (props) {
+		super(props)
+		this.state = {
+			films: [
+				{id: 1, titre: 'Titanic', date: '1997', auteur: 'James Cameron', genre: 'Drame', pays: 'USA', resume: 'Un bateau coule', note: 6, vu: true},
+				{id: 2, titre: 'Fast and Furious', date: '2001', auteur: 'Rob Cohen', genre: 'Action', pays: 'USA', resume:'Vin Diesel fait vroum', note: 5, vu: false},
+				{id: 3, titre: 'Le Parrain', date: '1972', auteur: 'Francis Ford Coppola', genre: 'Gangster', pays: 'USA', resume:'Histoire de la famille Corleone', note: 9, vu: false},
+				{id: 4, titre: 'Terminator', date: '1984', auteur: 'James Cameron', genre: 'Science Fiction', pays: 'USA', resume:'Schwarzy arrive nu sur Terre', note: 8, vu: true},
+				{id: 5, titre: 'Old Boy', date: '2003', auteur: 'Park Chan Wook', genre: 'Thriller', pays: 'Corée du Sud', resume:'Un homme veut se venger sévère', note: 9, vu: false},
+				{id: 6, titre: 'Steak', date: '2007', auteur: 'Quentin Dupieux', genre: 'Comédie', pays: 'France', resume:'Eric et Ramzy', note: 5, vu: false},
+				{id: 7, titre: 'Gran Torino', date: '2008', auteur: 'Clint Eastwood', genre: 'Drame', pays: 'USA', resume:'Un vieux raciste devient pote avec des corréens', note: 6, vu: false},
+				{id: 8, titre: 'Reservoir Dogs', date: '1992', auteur: 'Quentin Tarantino', genre: 'Gangster', pays: 'USA', resume:'Apprend tes couleurs de manière ludique !', note: 8, vu: true}
+			]
+		}
 	}
 
 }
--- src/components/oeuvreTerminees.js
+++ src/components/oeuvreTerminees.js
@@ -1,21 +1,8 @@
 import Oeuvre from './oeuvre.js'
-import * as React from 'react'
+import * as React from 'react';
 import Image from './Image.js'
-import './css/oeuvre.css'
 
 export default class OeuvreTerminees extends Oeuvre  {
-	constructor (props) {
-		super(props)
-		this.state = {
-			films: [
-				{id: 1, titre: 'Titanic', date: '1997', auteur: 'James Cameron', genre: 'Drame', pays: 'USA', resume: 'Un bateau coule'},
-				{id: 2, titre: 'Fast and Furious', date: 'Inconnu', auteur: 'Vin Diesel', genre: 'Vin Diesel', pays: 'USA', resume:'Vin Diesel'},
-				{id: 3, titre: 'jaimelimaclefilm', date: '2018', auteur: 'Venceslas Biri', genre: 'Moijaimebien', pays: 'France', resume:'compilercesttricher'},
-				{id: 4, titre: 'jaimelimaclefilm', date: '2018', auteur: 'Venceslas Biri', genre: 'Moijaimebien', pays: 'France', resume:'compilercesttricher'}
-			],
-			note:5
-		}
-	}
 
 		changeNote() {
 			if(this.state.note==10){
@@ -32,30 +19,32 @@ export default class OeuvreTerminees extends Oeuvre  {
 
 	render() {
 		const {films} = this.state;
+		const filmsVus = films.filter(i => i.vu == true);
 		const  content = <div className='Oeuvre'>
-			{films.map(film => {
+			{filmsVus.map(film => {
 				return <tr key={film.id}>
 				<div className = "oeuvre-wrap">
-				      <p className='Title'> {film.titre} </p>
+				      <h4> {film.titre} </h4>
+				      <p className='Date'> {film.date} </p>
 				      <div className = 'InfosOeuvre'>
-								<div className ='InfosImage'>
-									<Image/>
-								</div>
 								<div className = 'InfosTexte'>
-								  <p className='Date'> {film.date} </p>
 					      	<p className = 'Auteur'> Auteur : {film.auteur} </p>
 					      	<p className = 'Genre'>  Genre : {film.genre} </p>
 					      	<p className = 'Pays'> Origine :{film.pays} </p>
 					      	<p className = 'Resume'> Resume : {film.resume} </p>
 								</div>
-								<div className ='fonctions'>
-									<p className='note' onClick={this.changeNote.bind(this)}> {this.state.note} </p>
+								<div className ='InfosImage'>
+									<Image/>
 								</div>
 				      </div>
+							<div className ='fonctions'>
+							  <p className='note' onClick={this.changeNote.bind(this)}> {film.note} </p>
+							</div>
 			    </div>
 			    </tr>
 		 	})}
 	    </div>
+
 	    return content;
 	}
 }
