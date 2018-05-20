ba584f000cf240ffa72ffe0a59c83b44dda679e7
pfranc02 <pierre.franc75012@gmail.com>
2018-05-20 16:30:12 +0200

    Delete index.js

--- src/src/index.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App2';
-
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
