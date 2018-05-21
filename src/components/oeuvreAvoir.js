235b4938024594f6ad92f699766de8705c55576d
Aleqsian <alexiandu57@hotmail.fr>
2018-05-21 02:30:32 +0200

    jsp

--- src/components/oeuvreAvoir.js
+++ src/components/oeuvreAvoir.js
@@ -5,41 +5,42 @@ import Bouton from './Bouton.js'
 
 export default class OeuvreAvoir extends Oeuvre  {
 
-	changeViewed() {
-		this.films.setState({
+
+
+	changeViewed(props) {
+		this.setState({
 	    	vu: true
 	  	})
 	}
 
 	render() {
-			const {films} = this.state;
-			const filmsNVus = films.filter(i => i.vu == false);
-			const content = <div className='Oeuvre'>
-				{filmsNVus.map(film => {
-					return <tr key={film.id}>
-					<div className = "oeuvre-wrap">
-					      <h4> {film.titre} </h4>
-					      <p className='Date'> {film.date} </p>
-					      <div className = 'InfosOeuvre'>
-									<div className = 'InfosTexte'>
-						      	<p className = 'Auteur'> Auteur : {film.auteur} </p>
-						      	<p className = 'Genre'>  Genre : {film.genre} </p>
-						      	<p className = 'Pays'> Origine :{film.pays} </p>
-						      	<p className = 'Resume'> Resume : {film.resume} </p>
-									</div>
-									<div className ='InfosImage'>
-										<Image/>
-									</div>
-					      </div>
-								<div className ='Boutons'>
-									<Bouton onClick={this.changeViewed.bind(this)} />
+		const {films} = this.state;
+		const filmsNVus = films.filter(i => i.vu == false);
+		const content = <div className='Oeuvre'>
+			{filmsNVus.map(film => {
+				return <tr key={film.id}>
+				<div className = "oeuvre-wrap">
+				      <h4> {film.titre} </h4>
+				      <p className='Date'> {film.date} </p>
+				      <div className = 'InfosOeuvre'>
+								<div className = 'InfosTexte'>
+					      	<p className = 'Auteur'> Auteur : {film.auteur} </p>
+					      	<p className = 'Genre'>  Genre : {film.genre} </p>
+					      	<p className = 'Pays'> Origine :{film.pays} </p>
+					      	<p className = 'Resume'> Resume : {film.resume} </p>
 								</div>
-				    </div>
-				    </tr>
-			 	})}
-		    </div>
-
-		    return content;
+								<div className ='InfosImage'>
+									<Image/>
+								</div>
+				      </div>
+							<div className ='Boutons'>
+								<Bouton onClick={this.changeViewed.bind(this)} />
+							</div>
+			    </div>
+			    </tr>
+		 	})}
+		   </div>
+	   return content;
 	}
 	
 }
